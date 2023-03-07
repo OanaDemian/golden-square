a8680b936151da404d2049a0088cf54b7f484992
Oana <oana.demian@gmail.com>
2023-03-07 17:00:49 +0000

    returns entire text if the paragraph is 5 words or shorter

--- lib/make_snippet.rb
+++ lib/make_snippet.rb
@@ -1,5 +1,10 @@
 # A method called make_snippet that takes a string as an argument and returns the first five words and then a '...' if there are more than that.
 
 def make_snippet(text)
-  text.split(" ").length
+  paragraph_length = text.split(" ").length
+  if paragraph_length <= 5
+    text
+  else
+    text + "..."
+  end
 end
--- spec/make_snippet_spec.rb
+++ spec/make_snippet_spec.rb
@@ -6,8 +6,13 @@ RSpec.describe "make_snippet" do
     expect(result).to eq "I want nail test driven development."
   end
 
-  it "checks the number of words in the sentence" do
+  xit "returns the number of words in the sentence" do
     result =  make_snippet("I want nail test driven development.")
     expect(result).to eq(6)
+  end
+
+  it "returns the text if the paragraph is 5 words or shorter" do
+    result =  make_snippet("I like coding in Ruby.")
+    expect(result).to eq("I like coding in Ruby.")
   end
 end
