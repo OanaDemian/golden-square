e49a7c3fe2aa8ad923303dc924b55107b5354cd5
Oana <oana.demian@gmail.com>
2023-03-07 17:10:14 +0000

    refactored tests

--- spec/make_snippet_spec.rb
+++ spec/make_snippet_spec.rb
@@ -1,14 +1,14 @@
 require 'make_snippet'
 
 RSpec.describe "make_snippet" do
-  xit "returns a string" do
-    result = make_snippet("I want nail test driven development.")
-    expect(result).to eq "I want nail test driven development."
+  it "returns a string of one word" do
+    result = make_snippet("Ruby")
+    expect(result).to eq "Ruby"
   end
 
-  xit "returns the number of words in the sentence" do
-    result =  make_snippet("I want nail test driven development.")
-    expect(result).to eq(6)
+  it "returns the number of words in a paragraph containing less than 5 words" do
+    result =  make_snippet("test driven development")
+    expect(result.split(" ").length).to eq(3)
   end
 
   it "returns entire text if the paragraph is 5 words or shorter" do
